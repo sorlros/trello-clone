5aaf7b0e2761094eeec5850cee70366c6a5ad59a
sorlros <sorlros2047@gmail.com>
2023-11-21 15:59:41 +0900

    RandomImages from Unsplash

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -13,6 +13,7 @@ import { FormSubmit } from "./form-submit";
 import { Button } from "../ui/button";
 import { X } from "lucide-react";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -66,6 +67,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
